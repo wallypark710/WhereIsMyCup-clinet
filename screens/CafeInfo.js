cb7c377e9647bf232f925f8f226137fbce840a97
wallypark710 <wallypark710@gmail.com>
2019-02-14 15:31:29 +0900

    add body contents

--- screens/CafeInfo.js
+++ screens/CafeInfo.js
@@ -9,6 +9,7 @@ import {
   SafeAreaView,
   Button,
   TouchableOpacity,
+  Linking,
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import GoogleMap from './Map';
@@ -20,9 +21,90 @@ class CafeInfo extends Component {
     header: null,
   };
 
+  state = {
+    top3Tags: this.props.navigation.state.params.cafe.top3Tags || [],
+    openingHours: this.props.navigation.state.params.cafe.openingHours || [],
+    contact: this.props.navigation.state.params.cafe.contact || '',
+    convenience: this.props.navigation.state.params.cafe.convenience || '',
+    description: this.props.navigation.state.params.cafe.description || '',
+  };
+
   goToScreen() {
     this.props.navigation.navigate('Feedback', {
       cafe: this.props.navigation.state.params.cafe,
+    });
+  }
+
+  renderOpeningHours() {
+    return this.state.openingHours.length !== 0 ? (
+      <View style={{ margin: 10 }}>
+        <View>
+          <Text style={styles.bodyContentTitle}>Opening Hours</Text>
+        </View>
+        {this.state.openingHours.map((ele) => (
+          <Text>{ele.time}</Text>
+        ))}
+      </View>
+    ) : (
+      0
+    );
+  }
+
+  renderContact() {
+    return this.state.contact ? (
+      <View style={{ margin: 10 }}>
+        <View>
+          <Text style={styles.bodyContentTitle}>Contact</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            this.onPressCall(this.state.contact);
+          }}
+        >
+          <Text>{this.state.contact}</Text>
+        </TouchableOpacity>
+      </View>
+    ) : (
+      0
+    );
+  }
+
+  renderConvevience() {
+    return this.state.convenience ? (
+      <View style={{ margin: 10 }}>
+        <View>
+          <Text style={styles.bodyContentTitle}>Convenience</Text>
+        </View>
+        <Text>{this.state.convenience}</Text>
+      </View>
+    ) : (
+      0
+    );
+  }
+
+  renderDiscription() {
+    return this.state.description ? (
+      <View style={{ margin: 10 }}>
+        <View>
+          <Text style={styles.bodyContentTitle}>About</Text>
+        </View>
+        <Text>{this.state.description}</Text>
+      </View>
+    ) : (
+      0
+    );
+  }
+
+  onPressCall(phoneNumber) {
+    const url = `tel:+${phoneNumber}`;
+    Linking.canOpenURL(url).then(async (supported) => {
+      if (supported) {
+        try {
+          return await Linking.openURL(url);
+        } catch (e) {
+          return null;
+        }
+      }
     });
   }
 
@@ -81,13 +163,20 @@ class CafeInfo extends Component {
                           paddingHorizontal: 15,
                         }}
                       >
-                        어떠셨나요?
+                        Logo
                       </Text>
                     </TouchableOpacity>
                   </View>
 
                   <View style={styles.infoBody}>
-                    <View style={styles.bodyContents} />
+                    <View style={styles.bodyContents}>
+                      {[
+                        this.renderOpeningHours(),
+                        this.renderContact(),
+                        this.renderConvevience(),
+                        this.renderDiscription(),
+                      ].filter((ele) => ele !== 0)}
+                    </View>
                   </View>
                 </View>
               </View>
@@ -147,7 +236,7 @@ const styles = StyleSheet.create({
   },
   bodyContents: {
     width: width - 40,
-    height: 230,
+    minHeight: 130,
     marginBottom: 30,
     backgroundColor: 'white',
     marginTop: 20,
@@ -169,5 +258,10 @@ const styles = StyleSheet.create({
     width: width,
     height: width - 110,
     marginBottom: 20,
+  },
+  bodyContentTitle: {
+    fontSize: 15,
+    fontWeight: '700',
+    marginBottom: 5,
   },
 });
